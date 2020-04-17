9299311fff050d2d592fc40343c2d84f3d92be16
Spatel4 <57647348+SmitPatel19@users.noreply.github.com>
2020-04-16 21:20:10 -0400

    Create Final_Project.py

---
+++ Final_Project.py
@@ -0,0 +1,22 @@
+#The goal below is to build a web app that locates the nearest MBTA stop from a given input 
+
+#-----------------------Part 1----------------------------------------------------------------
+import urllib.request
+import json
+from pprint import pprint
+from json import load
+
+MAPQUEST_API_KEY = 'O1gGM8CKRPGplNw9tGa0zLl1GZUIYdsc'
+
+url = f'http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location=Babson%20College'
+f = urllib.request.urlopen(url)
+response_text = f.read().decode('utf-8')
+response_data = json.loads(response_text) #steralizing the data
+pprint(response_data)
+
+print(response_data["results"][0]["locations"][0]['postalCode']) #call specific pieces of info out
+
+#print(load(latLng))
+
+for items in response_data['locations']:
+    print(items['latLng'])
