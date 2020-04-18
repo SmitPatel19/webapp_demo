62e7d1bf45f4212ab9c0ef0969b1c235415ba239
Spatel4 <57647348+SmitPatel19@users.noreply.github.com>
2020-04-18 12:20:49 -0400

    Update Final_Project.py

--- Final_Project.py
+++ Final_Project.py
@@ -7,16 +7,30 @@ from pprint import pprint
 from json import load
 
 MAPQUEST_API_KEY = 'O1gGM8CKRPGplNw9tGa0zLl1GZUIYdsc'
+MAPQUEST_BASE_URL = 'http://www.mapquestapi.com/geocoding/v1/address'
 
 url = f'http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location=Babson%20College'
 f = urllib.request.urlopen(url)
+
+def get_json(url):
+    response_data = ...
+    return response_data
+
 response_text = f.read().decode('utf-8')
 response_data = json.loads(response_text) #steralizing the data
 pprint(response_data)
 
 print(response_data["results"][0]["locations"][0]['postalCode']) #call specific pieces of info out
 
-#print(load(latLng))
+def get_lat_long(place_name):
+    place = place_name.replace(' ', '%20')
+    url = f'{MAPQUEST_BASE_URL}?key={MAPQUEST_API_KEY}&location={place}'
+    print(url) 
+    place_json = get_json(url)
+    pprint(place_json)
+    lat = place_json[...][...] # modify this so you get the correct latitude
+    lon = place_json[...][...] # modify this so you get the correct longitude
 
-for items in response_data['results']:
-    print(items['latLng'])
+    return lat, lon
+
+
